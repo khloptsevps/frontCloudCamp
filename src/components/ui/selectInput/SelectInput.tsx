25667fa399ed11f49f3cc610af29327d650ee109
Khloptsev Petr <petr.khloptsev@fogstream.ru>
2023-06-13 14:40:41 +1000

    реализован селект выбора пола

--- src/components/ui/selectInput/SelectInput.tsx
+++ src/components/ui/selectInput/SelectInput.tsx
@@ -1,16 +1,41 @@
-import { useField } from 'formik';
+import { useField, useFormikContext } from 'formik';
+import Select from 'react-select';
 import styles from './SelectInput.module.scss';
 
-const SelectInput = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
+type Option = {
+  value: string;
+  label: string;
+};
+interface SelectInputProps {
+  name: string;
+  options: Option[];
+}
+
+const SelectInput = ({ name, options, ...props }: SelectInputProps) => {
+  const [field, meta] = useField(name);
+  const { setFieldValue } = useFormikContext();
+
+  const value = options.find((o) => o.value === field.value);
 
   return (
     <div className={styles.root}>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <select className={styles.select} {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
+      <div className={styles.label}>
+        <span>Sex</span>
+      </div>
+      <Select
+        inputId="field-sex"
+        options={options}
+        placeholder="Не выбрано"
+        components={{ IndicatorSeparator: () => null }}
+        onChange={(v) => setFieldValue(name, v?.value)}
+        value={value}
+        {...props}
+      />
+      <div className={styles.feedback}>
+        {meta.touched && meta.error ? (
+          <span className={styles.error}>{meta.error}</span>
+        ) : null}
+      </div>
     </div>
   );
 };
