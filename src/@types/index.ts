8eb82ce1c25252b0f8c495d9f613060f1e172dad
Khloptsev Petr <petr.khloptsev@fogstream.ru>
2023-06-15 19:22:01 +1000

    добавлены новые типы

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -9,6 +9,19 @@ export type FormDataFields = {
   aboutField: string;
 };
 
+export type PreparedFormData = {
+  phone: string;
+  email: string;
+  nickname: string;
+  name: string;
+  sername: string;
+  sex: string;
+  radioOption: number;
+  advantages: string[];
+  checkboxes: number[];
+  aboutField: string;
+};
+
 export type Checkbox = {
   key: string;
   checked: boolean;
@@ -36,3 +49,8 @@ export type FormStepTwo = Pick<
   'advantages' | 'checkboxes' | 'radioOption'
 >;
 export type FormStepThree = Pick<FormDataFields, 'aboutField'>;
+
+export interface FormState extends FormDataFields {
+  phone: string;
+  email: string;
+}
