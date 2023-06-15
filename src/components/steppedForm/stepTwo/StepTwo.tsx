045d93d364922420f9a86d56116841a7fec0ec09
Khloptsev Petr <petr.khloptsev@fogstream.ru>
2023-06-15 13:39:28 +1000

    добавлен checkboxArray

--- src/components/steppedForm/stepTwo/StepTwo.tsx
+++ src/components/steppedForm/stepTwo/StepTwo.tsx
@@ -1,13 +1,7 @@
-import { Checkbox, RadioButton } from 'components/ui';
+import { AdvantagesForm, CheckboxArray } from 'components';
+import { RadioButton } from 'components/ui';
 
 import styles from './StepTwo.module.scss';
-import { AdvantagesForm } from 'components';
-
-type Checkbox = {
-  id: string;
-  label: number;
-  name: string;
-};
 
 type Radio = {
   id: string;
@@ -16,29 +10,19 @@ type Radio = {
 };
 
 interface StepTwoProps {
-  checkboxes: Checkbox[];
   radioButtons: Radio[];
 }
 
-const StepTwo = ({ checkboxes, radioButtons }: StepTwoProps) => {
+const StepTwo = ({ radioButtons }: StepTwoProps) => {
   return (
     <div className={styles.root}>
       <div className={styles.advantages}>
         <h3>Advantages</h3>
         <AdvantagesForm name="advantages" />
       </div>
-      <div className={styles.checkboxGroup}>
+      <div className={styles.checkboxGroup} style={{ marginBottom: '24px' }}>
         <h3>Checkbox group</h3>
-        <div className={styles.checkboxes}>
-          {checkboxes?.map(({ id, label, name }) => (
-            <Checkbox
-              key={id}
-              id={`field-checkbox-group-option-${id}`}
-              name={name}
-              label={label}
-            />
-          ))}
-        </div>
+        <CheckboxArray name="checkboxes" />
       </div>
       <div className={styles.radios}>
         <h3>Radio group</h3>
