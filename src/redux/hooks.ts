8702ed479bdb25f94148d8f45ef6d050ed9aa9d0
Khloptsev Petr <petr.khloptsev@fogstream.ru>
2023-06-16 20:27:30 +1000

    добавил хук useFormData

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,6 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
+import { selectForm } from './formSlice/formSlice';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useFormData = () => {
+  const formData = useAppSelector(selectForm);
+
+  return { ...formData };
+};
