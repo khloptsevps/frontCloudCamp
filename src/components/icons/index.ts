99256a3da37e4cfc17defc02f7dff1ce6d4dc1f5
Khloptsev Petr <petr.khloptsev@fogstream.ru>
2023-06-16 17:57:19 +1000

    добавлены иконки

---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as CloseIcon } from './CloseIcon';
+export { default as ErrorIcon } from './ErrorIcon';
+export { default as SuccessIcon } from './SuccessIcon';
+export { default as DeleteButtonIcon } from './DeleteButtonIcon';
+export { default as CompleteStepIcon } from './CompleteStepIcon';
